e06d0ae7504e6d80a92fba6b75e6328ccf3dd741
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-05-20 16:57:21 +0300

    index signature

--- 04-ObjectsInTS/index.js
+++ 04-ObjectsInTS/index.js
@@ -30,4 +30,44 @@ let cat = {
 };
 //console.log(cat.name); error
 console.log(typeof cat);
+let post1 = {
+    title: "Post Uno",
+    content: "La Alegria",
+    date: new Date(),
+};
+let post2 = {
+    title: "Post Dos",
+    content: "La Alegria 2",
+    date: new Date(),
+};
+console.log(post2.date);
+let post3 = {
+    title: "Title tres",
+    content: "Bangaranga",
+    date: new Date(),
+    author: {
+        name: "Dara",
+        age: 27,
+    }
+};
+console.log(post3.author.age);
+let post4 = {
+    title: "Title cuatro?",
+    content: "Babayaga",
+    date: new Date(),
+    author: {
+        name: "Dara",
+        age: 27,
+    },
+    awards: {
+        awrd1: {
+            name: "First post award",
+            date: new Date(),
+        },
+        awrd2: {
+            name: "Second award - Yay!",
+            date: new Date(),
+        }
+    }
+};
 //# sourceMappingURL=index.js.map
--- 04-ObjectsInTS/index.ts
+++ 04-ObjectsInTS/index.ts
@@ -45,3 +45,83 @@ let cat: object = {
 //console.log(cat.name); error
 
 console.log(typeof cat);
+
+// NESTED OBJECTS
+
+type Post = {
+    title: string;
+    content: string;
+    date: Date;
+}
+
+let post1: Post = {
+    title: "Post Uno",
+    content: "La Alegria",
+    date: new Date(),
+}
+
+let post2 = {
+    title: "Post Dos",
+    content: "La Alegria 2",
+    date: new Date(),
+}
+console.log(post2.date);
+
+type Author = {
+    name: string;
+    age: number;
+}
+
+type NewPost = {
+    title: string;
+    content: string;
+    date: Date;
+    author: Author;
+}
+
+let post3: NewPost = {
+    title: "Title tres",
+    content: "Bangaranga",
+    date: new Date(),
+    author: {
+        name: "Dara",
+        age: 27,
+    }
+}
+console.log(post3.author.age);
+
+type NewPost2 = {
+    title: string;
+    content: string;
+    date: Date;
+    author: Author;
+    awards: Awards;
+}
+
+// index signature [key: string]
+type Awards = {
+    [key: string]: {
+    name: string;
+    date: Date;
+    }
+}
+
+let post4: NewPost2 = {
+ title: "Title cuatro?",
+    content: "Babayaga",
+    date: new Date(),
+    author: {
+        name: "Dara",
+        age: 27,
+    },
+    awards: {
+        awrd1: {
+            name: "First post award",
+            date: new Date(),
+        },
+        awrd2: {
+            name: "Second award - Yay!",
+            date: new Date(),
+        }
+    }
+}
