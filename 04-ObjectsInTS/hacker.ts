70ed9b765b625b1c25d04511e2d8696b598011a6
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-05-21 11:46:36 +0300

    hackerrank brackets

---
+++ 04-ObjectsInTS/hacker.ts
@@ -0,0 +1,41 @@
+function isBalanced(s: string): string {
+   // If the bracket count is not even: auto fail
+   // Add brackets into an array, until we find a right bracket (RB)
+   // When we find a RB pop the last element and compare, if they
+   // are equal continue, else break print NO.
+   // If no elements are left and the array is empty return yes
+ 
+   if(s.length % 2 != 0){
+       return "NO";
+   }
+ 
+  type leftBracketType = '(' | '[' | '{';
+ 
+   const bracketPairs = {
+       '{': '}',
+       '(': ')',
+       '[': ']',
+   }
+ 
+   // We need LB in an array
+   let leftBrackets: leftBracketType[] = [];
+   for(const bracket of s){
+       // We need a way to associate pairs (LB -> RB)
+       if(bracket in bracketPairs){
+           leftBrackets.push(bracket as leftBracketType);
+       } else{
+           // check if it is a RB
+           const bracket1 = leftBrackets.pop();
+           if(typeof bracket1 == "undefined"){
+               return "NO";
+           }
+           if(bracketPairs[bracket1] != bracket) {
+           return "NO";  
+               }
+        }
+   }
+    if(leftBrackets.length !== 0){
+       return "NO";
+    }  
+   return "YES";
+}
