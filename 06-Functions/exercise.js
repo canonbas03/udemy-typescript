5b5e2f86464541b13455d54c357c02ddd0984ef6
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-05-24 10:20:53 +0300

    exercise full

--- 06-Functions/exercise.js
+++ 06-Functions/exercise.js
@@ -15,4 +15,12 @@ const calculateArea = (width, height = 10) => {
 };
 console.log(calculateArea(5));
 console.log(calculateArea(5, 2));
+const checkStatus = (isActive = true) => {
+    if (isActive) {
+        return "Active";
+    }
+    return "Inactive";
+};
+console.log(checkStatus());
+console.log(checkStatus(false));
 //# sourceMappingURL=exercise.js.map
--- 06-Functions/exercise.ts
+++ 06-Functions/exercise.ts
@@ -14,9 +14,19 @@ console.log(greet2());
 console.log(greet2("John"));
 
 type AreaFunction = (width: number, height?: number) => number;
-
 const calculateArea: AreaFunction = (width, height = 10) => {
   return width * height;
 };
 console.log(calculateArea(5));
 console.log(calculateArea(5, 2));
+
+type StatusFunction = (isActive?: boolean) => string;
+const checkStatus: StatusFunction = (isActive = true) => {
+  if (isActive) {
+    return "Active";
+  }
+  return "Inactive";
+};
+
+console.log(checkStatus());
+console.log(checkStatus(false));
