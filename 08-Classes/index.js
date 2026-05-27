1b75e1e8ff8a1fe03697e572a0b4557130d2b1e6
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-05-27 15:00:20 +0300

    constructor

--- 08-Classes/index.js
+++ 08-Classes/index.js
@@ -9,5 +9,37 @@ class User {
 const user = new User();
 console.log(user);
 console.log(user.greet);
+// --- CONSTRUCTOR ---
+class User1 {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        return `${this.name} ${this.age}`;
+    }
+}
+const user2 = new User1("Jack", 34);
+console.log(user2.greet());
+// --- CLASS AS A TYPE ---
+const user3 = new User1("Miranda", 31);
+// --- OPTIONAL AND READONLY FIELDS ---
+class User2 {
+    name;
+    age;
+    lastName;
+    constructor(name, age, lastName) {
+        this.name = name;
+        this.age = age;
+        this.lastName = lastName;
+    }
+    greet() {
+        return `${this.name} ${this.age}`;
+    }
+}
+const user4 = new User1("Jack", 34);
+console.log(user2.greet());
 export {};
 //# sourceMappingURL=index.js.map
--- 08-Classes/index.ts
+++ 08-Classes/index.ts
@@ -11,3 +11,42 @@ class User {
 const user = new User();
 console.log(user);
 console.log(user.greet);
+
+// --- CONSTRUCTOR ---
+class User1 {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    return `${this.name} ${this.age}`;
+  }
+}
+
+const user2 = new User1("Jack", 34);
+console.log(user2.greet());
+
+// --- CLASS AS A TYPE ---
+const user3: User1 = new User1("Miranda", 31);
+
+// --- OPTIONAL AND READONLY FIELDS ---
+class User2 {
+  readonly name: string;
+  age: number;
+  lastName?: string | undefined;
+  constructor(name: string, age: number, lastName?: string) {
+    this.name = name;
+    this.age = age;
+    this.lastName = lastName;
+  }
+
+  greet() {
+    return `${this.name} ${this.age}`;
+  }
+}
+
+const user4 = new User1("Jack", 34);
+console.log(user2.greet());
