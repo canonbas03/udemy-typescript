92ce7aa5235891ba1aaf3505ba8aba76968f7514
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-06-05 10:44:10 +0300

    Base classes created

--- TASKS/school.js
+++ TASKS/school.js
@@ -1,17 +1,57 @@
 class School {
     name;
-    foundDate;
+    foundYear;
     address;
     building;
     teachers;
     classes;
-    constructor(name, foundDate, address = "Make it Address later", building = "Make it a Building", teachers = ["We can have teacher class later"], classes = ["We can have a classes class later"]) {
+    constructor(name, foundYear, address = "Make it Address later", building = "Make it a Building", teachers = ["We can have teacher class later"], classes = ["We can have a classes class later"]) {
         this.name = name;
-        this.foundDate = foundDate;
+        this.foundYear = foundYear;
         this.address = address;
         this.building = building;
         this.teachers = teachers;
         this.classes = classes;
+    }
+}
+class Building {
+    size;
+    floors;
+    constructor(size, floors = [
+        "list of all floors maybe we need a floor class later",
+    ]) {
+        this.size = size;
+        this.floors = floors;
+    }
+}
+class Floor {
+    classrooms;
+    teacherRooms;
+    toiletCount;
+    constructor(classrooms = ["Use classroom class later"], teacherRooms = ["Use teacherRooms class later"], toiletCount) {
+        this.classrooms = classrooms;
+        this.teacherRooms = teacherRooms;
+        this.toiletCount = toiletCount;
+    }
+}
+class Classroom {
+    name;
+    sizeInSqM;
+    maxStudents;
+    constructor(name, sizeInSqM, maxStudents) {
+        this.name = name;
+        this.sizeInSqM = sizeInSqM;
+        this.maxStudents = maxStudents;
+    }
+}
+class TeacherRoom {
+    sizeInSqM;
+    teachers;
+    maxTeachers;
+    constructor(sizeInSqM, teachers = ["Teachers class later"], maxTeachers) {
+        this.sizeInSqM = sizeInSqM;
+        this.teachers = teachers;
+        this.maxTeachers = maxTeachers;
     }
 }
 export {};
--- TASKS/school.ts
+++ TASKS/school.ts
@@ -1,10 +1,43 @@
 class School {
   constructor(
     public name: string,
-    public foundDate: Date,
+    public readonly foundYear: number,
     public address: string = "Make it Address later",
     public building: string = "Make it a Building",
     public teachers: string[] = ["We can have teacher class later"],
     public classes: string[] = ["We can have a classes class later"],
+  ) {}
+}
+
+class Building {
+  constructor(
+    public size: number,
+    public readonly floors: string[] = [
+      "list of all floors maybe we need a floor class later",
+    ],
+  ) {}
+}
+
+class Floor {
+  constructor(
+    public classrooms: string[] = ["Use classroom class later"],
+    public teacherRooms: string[] = ["Use teacherRooms class later"],
+    public toiletCount: number,
+  ) {}
+}
+
+class Classroom {
+  constructor(
+    public name: string,
+    public readonly sizeInSqM: number,
+    public maxStudents: number,
+  ) {}
+}
+
+class TeacherRoom {
+  constructor(
+    public readonly sizeInSqM: number,
+    public teachers: string[] = ["Teachers class later"],
+    public maxTeachers: number,
   ) {}
 }
