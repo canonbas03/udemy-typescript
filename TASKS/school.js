fdd63b695d9359e24afe327a51803bb26932f75f
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-06-05 14:09:43 +0300

    created new instances of the classes

--- TASKS/school.js
+++ TASKS/school.js
@@ -1,3 +1,21 @@
+var Subjects;
+(function (Subjects) {
+    Subjects["Math"] = "math";
+    Subjects["Bulgarian"] = "bulgarian";
+    Subjects["English"] = "english";
+    Subjects["Physics"] = "physics";
+    Subjects["Chemistry"] = "chemistry";
+    Subjects["Literature"] = "literature";
+    Subjects["PhysicalEducation"] = "physical education";
+})(Subjects || (Subjects = {}));
+class Class {
+    name;
+    students;
+    constructor(name, students) {
+        this.name = name;
+        this.students = students;
+    }
+}
 class School {
     name;
     foundYear;
@@ -5,7 +23,7 @@ class School {
     building;
     teachers;
     classes;
-    constructor(name, foundYear, address = "Make it Address later", building = "Make it a Building", teachers = ["We can have teacher class later"], classes = ["We can have a classes class later"]) {
+    constructor(name, foundYear, address, building, teachers, classes) {
         this.name = name;
         this.foundYear = foundYear;
         this.address = address;
@@ -14,13 +32,79 @@ class School {
         this.classes = classes;
     }
 }
+class Teacher {
+    name;
+    age;
+    sex;
+    subject;
+    classes;
+    constructor(name, age, sex, subject, classes) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.subject = subject;
+        this.classes = classes;
+    }
+}
+const address = {
+    city: "Varna",
+    street: "Primorski",
+    number: 6,
+};
+// Students
+const student1 = {
+    name: "John",
+    age: 14,
+    gradesOnSubjects: [
+        [Subjects.Bulgarian, 6],
+        [Subjects.Chemistry, 3],
+    ],
+};
+const student2 = {
+    name: "John",
+    age: 14,
+    gradesOnSubjects: [
+        [Subjects.Bulgarian, 6],
+        [Subjects.Chemistry, 3],
+    ],
+};
+// Classes
+const class1 = new Class("8 A class", [student1, student2]);
+const class2 = new Class("8 B class", [student1, student2]);
+// Classrooms
+const classroom1 = {
+    classStudents: class1,
+    sizeInSqM: 40,
+    maxStudents: 20,
+};
+const classroom2 = {
+    classStudents: class2,
+    sizeInSqM: 39,
+    maxStudents: 19,
+};
+// Floors
+const floor1 = {
+    classrooms: [classroom1, classroom2],
+    teacherRooms: [],
+    toiletCount: 2,
+};
+const building = {
+    sizeInSqM: 300,
+    floors: [floor1],
+};
+// Teacher rooms
+const teacher1 = new Teacher("Anna", 35, "female", Subjects.Bulgarian, [class1, class2]);
+const teacherRoom1 = {
+    sizeInSqM: 50,
+    teachers: [],
+    maxTeachers: 0,
+};
+const baseSchool = new School("School Name", 2026, address, building, [teacher1], [class1, class2]);
 class Building {
-    size;
+    sizeInSqM;
     floors;
-    constructor(size, floors = [
-        "list of all floors maybe we need a floor class later",
-    ]) {
-        this.size = size;
+    constructor(sizeInSqM, floors) {
+        this.sizeInSqM = sizeInSqM;
         this.floors = floors;
     }
 }
@@ -28,18 +112,18 @@ class Floor {
     classrooms;
     teacherRooms;
     toiletCount;
-    constructor(classrooms = ["Use classroom class later"], teacherRooms = ["Use teacherRooms class later"], toiletCount) {
+    constructor(classrooms, teacherRooms, toiletCount) {
         this.classrooms = classrooms;
         this.teacherRooms = teacherRooms;
         this.toiletCount = toiletCount;
     }
 }
 class Classroom {
-    name;
+    classStudents;
     sizeInSqM;
     maxStudents;
-    constructor(name, sizeInSqM, maxStudents) {
-        this.name = name;
+    constructor(classStudents, sizeInSqM, maxStudents) {
+        this.classStudents = classStudents;
         this.sizeInSqM = sizeInSqM;
         this.maxStudents = maxStudents;
     }
@@ -48,10 +132,20 @@ class TeacherRoom {
     sizeInSqM;
     teachers;
     maxTeachers;
-    constructor(sizeInSqM, teachers = ["Teachers class later"], maxTeachers) {
+    constructor(sizeInSqM, teachers, maxTeachers) {
         this.sizeInSqM = sizeInSqM;
         this.teachers = teachers;
         this.maxTeachers = maxTeachers;
+    }
+}
+class Student {
+    name;
+    age;
+    gradesOnSubjects;
+    constructor(name, age, gradesOnSubjects) {
+        this.name = name;
+        this.age = age;
+        this.gradesOnSubjects = gradesOnSubjects;
     }
 }
 export {};
--- TASKS/school.ts
+++ TASKS/school.ts
@@ -1,34 +1,139 @@
+enum Subjects {
+  Math = "math",
+  Bulgarian = "bulgarian",
+  English = "english",
+  Physics = "physics",
+  Chemistry = "chemistry",
+  Literature = "literature",
+  PhysicalEducation = "physical education",
+}
+
+class Class {
+  constructor(
+    public name: string,
+    public students: Student[],
+  ) {}
+}
+
 class School {
   constructor(
     public name: string,
     public readonly foundYear: number,
-    public address: string = "Make it Address later",
-    public building: string = "Make it a Building",
-    public teachers: string[] = ["We can have teacher class later"],
-    public classes: string[] = ["We can have a classes class later"],
+    public address: Address,
+    public building: Building,
+    public teachers: Teacher[],
+    public classes: Class[],
   ) {}
 }
 
+class Teacher {
+  constructor(
+    public name: string,
+    public age: number,
+    public sex: "male" | "female",
+    public subject: Subjects,
+    public classes: Class[],
+  ) {}
+}
+
+const address: Address = {
+  city: "Varna",
+  street: "Primorski",
+  number: 6,
+};
+
+// Students
+const student1: Student = {
+  name: "John",
+  age: 14,
+  gradesOnSubjects: [
+    [Subjects.Bulgarian, 6],
+    [Subjects.Chemistry, 3],
+  ],
+};
+
+const student2: Student = {
+  name: "John",
+  age: 14,
+  gradesOnSubjects: [
+    [Subjects.Bulgarian, 6],
+    [Subjects.Chemistry, 3],
+  ],
+};
+
+// Classes
+const class1: Class = new Class("8 A class", [student1, student2]);
+const class2: Class = new Class("8 B class", [student1, student2]);
+
+// Classrooms
+const classroom1: Classroom = {
+  classStudents: class1,
+  sizeInSqM: 40,
+  maxStudents: 20,
+};
+
+const classroom2: Classroom = {
+  classStudents: class2,
+  sizeInSqM: 39,
+  maxStudents: 19,
+};
+// Floors
+const floor1: Floor = {
+  classrooms: [classroom1, classroom2],
+  teacherRooms: [],
+  toiletCount: 2,
+};
+const building: Building = {
+  sizeInSqM: 300,
+  floors: [floor1],
+};
+
+// Teacher rooms
+const teacher1: Teacher = new Teacher(
+  "Anna",
+  35,
+  "female",
+  Subjects.Bulgarian,
+  [class1, class2],
+);
+const teacherRoom1: TeacherRoom = {
+  sizeInSqM: 50,
+  teachers: [],
+  maxTeachers: 0,
+};
+
+const baseSchool: School = new School(
+  "School Name",
+  2026,
+  address,
+  building,
+  [teacher1],
+  [class1, class2],
+);
+type Address = {
+  city: string;
+  street: string;
+  number: number;
+};
+
 class Building {
   constructor(
-    public size: number,
-    public readonly floors: string[] = [
-      "list of all floors maybe we need a floor class later",
-    ],
+    public sizeInSqM: number,
+    public readonly floors: Floor[],
   ) {}
 }
 
 class Floor {
   constructor(
-    public classrooms: string[] = ["Use classroom class later"],
-    public teacherRooms: string[] = ["Use teacherRooms class later"],
+    public classrooms: Classroom[],
+    public teacherRooms: TeacherRoom[],
     public toiletCount: number,
   ) {}
 }
 
 class Classroom {
   constructor(
-    public name: string,
+    public classStudents: Class,
     public readonly sizeInSqM: number,
     public maxStudents: number,
   ) {}
@@ -37,7 +142,15 @@ class Classroom {
 class TeacherRoom {
   constructor(
     public readonly sizeInSqM: number,
-    public teachers: string[] = ["Teachers class later"],
+    public teachers: Teacher[],
     public maxTeachers: number,
+  ) {}
+}
+
+class Student {
+  constructor(
+    public name: string,
+    public age: number,
+    public gradesOnSubjects: [Subjects, number][],
   ) {}
 }
