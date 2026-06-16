418da25287ca88966a04c3deebcfa3c03faf148e
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-06-16 14:09:06 +0300

    strict typing

--- 12-DecoratorsStage3/index.js
+++ 12-DecoratorsStage3/index.js
@@ -32,6 +32,62 @@ var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, 
     if (target) Object.defineProperty(target, contextIn.name, descriptor);
     done = true;
 };
+// SIMPLE DECORATOR
+function mySimpleDecorator(originalMethod, context) {
+    function wrapper(...args) {
+        console.log("Arguments received:", args);
+        originalMethod(...args);
+    }
+    return wrapper;
+}
+let Robot = (() => {
+    let _instanceExtraInitializers = [];
+    let _sayHello_decorators;
+    return class Robot {
+        static {
+            const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            _sayHello_decorators = [mySimpleDecorator];
+            __esDecorate(this, null, _sayHello_decorators, { kind: "method", name: "sayHello", static: false, private: false, access: { has: obj => "sayHello" in obj, get: obj => obj.sayHello }, metadata: _metadata }, null, _instanceExtraInitializers);
+            if (_metadata) Object.defineProperty(this, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        }
+        sayHello(name) {
+            console.log(`Hello, ${name}!`);
+        }
+        constructor() {
+            __runInitializers(this, _instanceExtraInitializers);
+        }
+    };
+})();
+const bot = new Robot();
+bot.sayHello("Alice");
+// USING THIS
+console.log("\n", "// USING THIS");
+function mySimpleDecorator2(originalMethod, context) {
+    function wrapper(...args) {
+        originalMethod.call(this, ...args);
+    }
+    return wrapper;
+}
+let Robot2 = (() => {
+    let _instanceExtraInitializers = [];
+    let _sayModel_decorators;
+    return class Robot2 {
+        static {
+            const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            _sayModel_decorators = [mySimpleDecorator2];
+            __esDecorate(this, null, _sayModel_decorators, { kind: "method", name: "sayModel", static: false, private: false, access: { has: obj => "sayModel" in obj, get: obj => obj.sayModel }, metadata: _metadata }, null, _instanceExtraInitializers);
+            if (_metadata) Object.defineProperty(this, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        }
+        model = (__runInitializers(this, _instanceExtraInitializers), "X-100");
+        sayModel() {
+            console.log(`My model is ${this.model}`);
+        }
+    };
+})();
+const bot2 = new Robot2();
+bot2.sayModel();
+// --- REPLACEMENT FUNCTION ---
+console.log("\n");
 function methodLogger(originalMethod, _context) {
     console.log("Decorator Invoked");
     //! The replacement function replaces the original method and when the original method is invoken then the replacement method is invoked instead of the original greet method.
@@ -67,5 +123,149 @@ let Person = (() => {
 })();
 let user = new Person("John");
 user.greet("Hello");
+// --- ADD INITIALIZER TO GREET ---
+function methodLogger2(originalMethod, context) {
+    //! STEP 2.1, CREATE VARIABLE FOR METHOD NAME
+    const methodName = context.name;
+    //! STEP 2.3: Add CONDITION FOR INITIALIZER
+    if (context.private) {
+        throw new Error(`'bound' cannot decorate private properties like ${methodName}.`);
+    }
+    //! STEP 2.2: ADD INITIALIZER
+    // Runs inside the constructor of Person when an instance is created.
+    // Binds the method to the instance (this), ensuring correct context.
+    // Equivalent to manually binding inside the constructor:
+    // typescript
+    context.addInitializer(function () {
+        this[methodName] = this[methodName].bind(this);
+    });
+    function replacementMethod(...args) {
+        console.log("Invocation Started");
+        const result = originalMethod.call(this, ...args);
+        console.log("Invocation ended");
+        return result;
+    }
+    return replacementMethod;
+}
+let Person2 = (() => {
+    let _instanceExtraInitializers = [];
+    let _greet_decorators;
+    return class Person2 {
+        static {
+            const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            _greet_decorators = [methodLogger2];
+            __esDecorate(this, null, _greet_decorators, { kind: "method", name: "greet", static: false, private: false, access: { has: obj => "greet" in obj, get: obj => obj.greet }, metadata: _metadata }, null, _instanceExtraInitializers);
+            if (_metadata) Object.defineProperty(this, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        }
+        name = __runInitializers(this, _instanceExtraInitializers);
+        constructor(name) {
+            this.name = name;
+            //! STEP 3: THIS IS HOW YOU WOULD HAVE DONE IT IN JAVASCRIPT CONSTRUCTOR
+            // this.greet = this.greet.bind(this);
+        }
+        greet(greeting) {
+            //! PART OF STEP 1
+            //* Console dir is a method that displays a list of the properties of the specified JavaScript object
+            console.dir(this);
+            console.log(` ${greeting}, ${this.name}`);
+        }
+    };
+})();
+let user2 = new Person2("John");
+user.greet("Hello");
+//! STEP 1: SEE THE PROBLEM
+//! There will be a problem if greet is called as a standalone function
+//! Reassigning greet to a separate variable to showcase the problem with the this keyword. You will see that this keword points to the Person class
+const greet = user2.greet;
+greet("Morning");
+// --- BOUND ---
+function methodLogger3(originalMethod, _context) {
+    function replacementMethod(...args) {
+        console.log("Invocation Started");
+        const result = originalMethod.call(this, ...args);
+        console.log("Invocation ended");
+        return result;
+    }
+    return replacementMethod;
+}
+function bound(_originalMethod, context) {
+    const methodName = context.name;
+    if (context.private) {
+        throw new Error(`'bound' cannot decorate private properties like ${methodName}.`);
+    }
+    context.addInitializer(function () {
+        this[methodName] = this[methodName].bind(this);
+    });
+}
+let Person3 = (() => {
+    let _instanceExtraInitializers = [];
+    let _greet_decorators;
+    return class Person3 {
+        static {
+            const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            _greet_decorators = [bound, methodLogger3];
+            __esDecorate(this, null, _greet_decorators, { kind: "method", name: "greet", static: false, private: false, access: { has: obj => "greet" in obj, get: obj => obj.greet }, metadata: _metadata }, null, _instanceExtraInitializers);
+            if (_metadata) Object.defineProperty(this, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        }
+        name = __runInitializers(this, _instanceExtraInitializers);
+        constructor(name) {
+            this.name = name;
+        }
+        greet(greeting) {
+            console.dir(this);
+            console.log(` ${greeting}, ${this.name}`);
+        }
+    };
+})();
+let user3 = new Person3("John");
+user3.greet("Hello");
+const greet3 = user3.greet;
+greet3("Morning");
+// --- STRICT TYPING DECORATORS ---
+//! STEP 2: Or second alternative could be that we use Generics, which makes the decorator strictly typed but increases the complexity of the code.
+function methodLogger4(logPrefix) {
+    return function (originalMethod, _context) {
+        return function (...args) {
+            console.log(`${logPrefix}: Invocation Started`);
+            const result = originalMethod.call(this, ...args);
+            console.log(`${logPrefix}: Invocation ended`);
+            return result;
+        };
+    };
+}
+//! STEP 1: We can use simple Type declaration of Function, here this will remain as ANY
+function bound4(_originalMethod, context) {
+    const methodName = String(context.name);
+    if (context.private) {
+        throw new Error(`'bound' cannot decorate private properties like ${methodName}.`);
+    }
+    context.addInitializer(function () {
+        this[methodName] = this[methodName].bind(this);
+    });
+}
+let Person4 = (() => {
+    let _instanceExtraInitializers = [];
+    let _greet_decorators;
+    return class Person4 {
+        static {
+            const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            _greet_decorators = [bound, methodLogger4("LOG")];
+            __esDecorate(this, null, _greet_decorators, { kind: "method", name: "greet", static: false, private: false, access: { has: obj => "greet" in obj, get: obj => obj.greet }, metadata: _metadata }, null, _instanceExtraInitializers);
+            if (_metadata) Object.defineProperty(this, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        }
+        name = __runInitializers(this, _instanceExtraInitializers);
+        constructor(name) {
+            this.name = name;
+        }
+        greet(greeting) {
+            console.dir(this);
+            console.log(` ${greeting}, ${this.name}`);
+        }
+    };
+})();
+let user4 = new Person4("John");
+user.greet("Hello");
+const greet4 = user.greet;
+greet("Morning");
 export {};
 //# sourceMappingURL=index.js.map
--- 12-DecoratorsStage3/index.ts
+++ 12-DecoratorsStage3/index.ts
@@ -1,3 +1,48 @@
+// SIMPLE DECORATOR
+function mySimpleDecorator(originalMethod: any, context: any) {
+  function wrapper(...args: any[]) {
+    console.log("Arguments received:", args);
+    originalMethod(...args);
+  }
+
+  return wrapper;
+}
+
+class Robot {
+  @mySimpleDecorator
+  sayHello(name: string) {
+    console.log(`Hello, ${name}!`);
+  }
+}
+
+const bot = new Robot();
+bot.sayHello("Alice");
+
+// USING THIS
+console.log("\n", "// USING THIS");
+
+function mySimpleDecorator2(originalMethod: any, context: any) {
+  function wrapper(this: any, ...args: any[]) {
+    originalMethod.call(this, ...args);
+  }
+  return wrapper;
+}
+
+class Robot2 {
+  model = "X-100";
+
+  @mySimpleDecorator2
+  sayModel() {
+    console.log(`My model is ${this.model}`);
+  }
+}
+
+const bot2 = new Robot2();
+bot2.sayModel();
+
+// --- REPLACEMENT FUNCTION ---
+console.log("\n");
+
 function methodLogger(originalMethod: any, _context: any) {
   console.log("Decorator Invoked");
   //! The replacement function replaces the original method and when the original method is invoken then the replacement method is invoked instead of the original greet method.
@@ -24,3 +69,153 @@ class Person {
 
 let user: Person = new Person("John");
 user.greet("Hello");
+
+// --- ADD INITIALIZER TO GREET ---
+function methodLogger2(originalMethod: any, context: any) {
+  //! STEP 2.1, CREATE VARIABLE FOR METHOD NAME
+  const methodName = context.name;
+
+  //! STEP 2.3: Add CONDITION FOR INITIALIZER
+  if (context.private) {
+    throw new Error(
+      `'bound' cannot decorate private properties like ${methodName as string}.`,
+    );
+  }
+
+  //! STEP 2.2: ADD INITIALIZER
+  // Runs inside the constructor of Person when an instance is created.
+  // Binds the method to the instance (this), ensuring correct context.
+  // Equivalent to manually binding inside the constructor:
+  // typescript
+
+  context.addInitializer(function (this: any) {
+    this[methodName] = this[methodName].bind(this);
+  });
+
+  function replacementMethod(this: any, ...args: any[]) {
+    console.log("Invocation Started");
+    const result = originalMethod.call(this, ...args);
+    console.log("Invocation ended");
+    return result;
+  }
+  return replacementMethod;
+}
+
+class Person2 {
+  constructor(public name: string) {
+    //! STEP 3: THIS IS HOW YOU WOULD HAVE DONE IT IN JAVASCRIPT CONSTRUCTOR
+    // this.greet = this.greet.bind(this);
+  }
+
+  @methodLogger2
+  greet(greeting: string) {
+    //! PART OF STEP 1
+    //* Console dir is a method that displays a list of the properties of the specified JavaScript object
+    console.dir(this);
+    console.log(` ${greeting}, ${this.name}`);
+  }
+}
+
+let user2: Person2 = new Person2("John");
+user.greet("Hello");
+//! STEP 1: SEE THE PROBLEM
+//! There will be a problem if greet is called as a standalone function
+//! Reassigning greet to a separate variable to showcase the problem with the this keyword. You will see that this keword points to the Person class
+const greet = user2.greet;
+greet("Morning");
+
+// --- BOUND ---
+function methodLogger3(originalMethod: any, _context: any) {
+  function replacementMethod(this: any, ...args: any[]) {
+    console.log("Invocation Started");
+    const result = originalMethod.call(this, ...args);
+    console.log("Invocation ended");
+    return result;
+  }
+  return replacementMethod;
+}
+
+function bound(_originalMethod: any, context: any) {
+  const methodName = context.name;
+
+  if (context.private) {
+    throw new Error(
+      `'bound' cannot decorate private properties like ${methodName as string}.`,
+    );
+  }
+
+  context.addInitializer(function (this: any) {
+    this[methodName] = this[methodName].bind(this);
+  });
+}
+
+class Person3 {
+  constructor(public name: string) {}
+
+  @bound
+  @methodLogger3
+  greet(greeting: string) {
+    console.dir(this);
+    console.log(` ${greeting}, ${this.name}`);
+  }
+}
+
+let user3: Person3 = new Person3("John");
+user3.greet("Hello");
+
+const greet3 = user3.greet;
+greet3("Morning");
+
+// --- STRICT TYPING DECORATORS ---
+//! STEP 2: Or second alternative could be that we use Generics, which makes the decorator strictly typed but increases the complexity of the code.
+function methodLogger4<This, Args extends any[], Return>(logPrefix: string) {
+  return function (
+    originalMethod: (this: This, ...args: Args) => Return,
+    _context: ClassMethodDecoratorContext<
+      This,
+      (this: This, ...args: Args) => Return
+    >,
+  ) {
+    return function (this: This, ...args: Args): Return {
+      console.log(`${logPrefix}: Invocation Started`);
+      const result = originalMethod.call(this, ...args);
+      console.log(`${logPrefix}: Invocation ended`);
+      return result;
+    };
+  };
+}
+
+//! STEP 1: We can use simple Type declaration of Function, here this will remain as ANY
+function bound4(
+  _originalMethod: Function,
+  context: ClassMethodDecoratorContext,
+) {
+  const methodName = String(context.name);
+
+  if (context.private) {
+    throw new Error(
+      `'bound' cannot decorate private properties like ${methodName as string}.`,
+    );
+  }
+
+  context.addInitializer(function (this: any) {
+    this[methodName] = this[methodName].bind(this);
+  });
+}
+
+class Person4 {
+  constructor(public name: string) {}
+
+  @bound
+  @methodLogger4("LOG")
+  greet(greeting: string) {
+    console.dir(this);
+    console.log(` ${greeting}, ${this.name}`);
+  }
+}
+
+let user4: Person4 = new Person4("John");
+user.greet("Hello");
+
+const greet4 = user.greet;
+greet("Morning");
