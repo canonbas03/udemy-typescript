6ebbc5e10f631dc4028759173027223c6ef78907
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-06-19 10:27:37 +0300

    field in a prototype

--- 12-DecoratorsStage3/Manager.js
+++ 12-DecoratorsStage3/Manager.js
@@ -1,18 +1,104 @@
+/*
+
 // Class decorator
-//@PrintDecoratorData
+@PrintDecoratorData
 class Manager {
-    task = "Simple task";
-    project = "Simple project";
-    constructor() {
-        console.log("Initializing the manager class");
-    }
+  task: string = "Simple task";
+  project: string = "Simple project";
+
+  constructor() {
+    console.log("Initializing the manager class");
+  }
 }
-function PrintDecoratorData(value, context) { }
+
+function PrintDecoratorData(value: Function, context: ClassDecoratorContext) {
+  console.log("Value: ");
+  console.log(value);
+
+  console.log("Class Decorator Context:");
+  console.log(context);
+}
+
 // Calling the constructor
 console.log("// Calling the constructor");
-new Manager();
 const manager = new Manager();
-console.log("// Manager logged:");
+
+console.log("// Manager logging:");
+console.log(manager);
+
+// */
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.unshift(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.unshift(_);
+            else descriptor[key] = _;
+        }
+    }
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
+};
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
+//*
+// Class decorator
+let Manager = (() => {
+    let _classDecorators = [withEmploymentDateOnPrototype];
+    let _classDescriptor;
+    let _classExtraInitializers = [];
+    let _classThis;
+    var Manager = class {
+        static { _classThis = this; }
+        static {
+            const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
+            Manager = _classThis = _classDescriptor.value;
+            if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+            __runInitializers(_classThis, _classExtraInitializers);
+        }
+        task = "Simple task";
+        project = "Simple project";
+        constructor() {
+            console.log("Initializing the manager class");
+        }
+    };
+    return Manager = _classThis;
+})();
+function PrintDecoratorData(value, context) {
+    console.log("Value: ");
+    console.log(value);
+    console.log("Class Decorator Context:");
+    console.log(context);
+}
+function withEmploymentDateOnPrototype(value, context) {
+    value.prototype.employmentDateOnPrototype = new Date().toString();
+}
+// Calling the constructor
+console.log("// Calling the constructor");
+const manager = new Manager();
+console.log("// Manager logging:");
 console.log(manager);
 export {};
+// */
 //# sourceMappingURL=Manager.js.map
--- 12-DecoratorsStage3/Manager.ts
+++ 12-DecoratorsStage3/Manager.ts
@@ -1,30 +1,64 @@
+/*
+
 // Class decorator
 @PrintDecoratorData
 class Manager {
   task: string = "Simple task";
   project: string = "Simple project";
 
-  // constructor() {
-  //   console.log("Initializing the manager class");
-  // }
+  constructor() {
+    console.log("Initializing the manager class");
+  }
 }
 
 function PrintDecoratorData(value: Function, context: ClassDecoratorContext) {
   console.log("Value: ");
   console.log(value);
 
-  console.log("classDecoratorContext:");
+  console.log("Class Decorator Context:");
   console.log(context);
 }
 
 // Calling the constructor
 console.log("// Calling the constructor");
-new Manager();
-
 const manager = new Manager();
 
-console.log("// Manager logged:");
+console.log("// Manager logging:");
 console.log(manager);
 
-console.log("Printing");
-manager;
+// */
+
+//*
+// Class decorator
+@withEmploymentDateOnPrototype
+class Manager {
+  task: string = "Simple task";
+  project: string = "Simple project";
+
+  constructor() {
+    console.log("Initializing the manager class");
+  }
+}
+
+function PrintDecoratorData(value: Function, context: ClassDecoratorContext) {
+  console.log("Value: ");
+  console.log(value);
+
+  console.log("Class Decorator Context:");
+  console.log(context);
+}
+
+function withEmploymentDateOnPrototype(
+  value: Function,
+  context: ClassDecoratorContext,
+) {
+  value.prototype.employmentDateOnPrototype = new Date().toString();
+}
+
+// Calling the constructor
+console.log("// Calling the constructor");
+const manager = new Manager();
+
+console.log("// Manager logging:");
+console.log(manager);
+// */
