dbabbe4050551670601be89ec5908b3f7c9dcef5
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-07-13 14:26:09 +0300

    // Indexed access types

--- 16-CreatingTypesFromTypes/index.ts
+++ 16-CreatingTypesFromTypes/index.ts
@@ -63,7 +63,7 @@ console.log(getProperty(user, "age")); // ✅ 30
 // console.log(getProperty(user, "email")); // ❌ Error: Argument of type '"email"' is not assignable
 //*/
 
-//*/
+/*/
 // TYPE OFF
 //! Using typeof to Create a Type from a Variable
 const user = {
@@ -103,4 +103,39 @@ function getMessage(status: StatusKeys): string {
 }
 
 console.log(getMessage("success")); // ✅ "Operation successful"
+//*/
+
+//*/
+// Indexed access types
+type User = {
+  id: number;
+  name: string;
+  age: number;
+};
+
+type UserNameType = User["name"]; // Extracts `string`
+
+type UserIDType = User["id"]; // Extracts `number`
+
+type UserNameOrAge = User["name" | "age"]; // string | number
+
+type UserValues = User[keyof User]; // string | number all value types
+
+const userName: UserNameType = "Alice"; // ✅ Allowed
+// const invalidUserName: UserNameType = 42; // ❌ Error: Type 'number' is not assignable to 'string'
+
+//! Using Indexed Access Types in Function
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+const userData: User = { id: 1, name: "Bob", age: 25 };
+
+const userName2: User["name"] = getProperty(userData, "name"); // ✅ Works fine
+const userAge: User["age"] = getProperty(userData, "age"); // ✅ Works fine
+
+// EXAMPLES
+type GiraffeHeight = Giraffe["height"]; // number
+type LionIsMale = Lion["_isMale"]; // boolean | undefined
+
 //*/
