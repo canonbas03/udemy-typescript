2acc978cb48d93c72370008498f0b241ef4c5a52
Salih-Dzhan Yonbash <166402031+canonbas03@users.noreply.github.com>
2026-07-13 13:45:55 +0300

    // KEYOFF OPERATOR

--- 16-CreatingTypesFromTypes/index.ts
+++ 16-CreatingTypesFromTypes/index.ts
@@ -1,4 +1,4 @@
-//*/
+/*/
 // GENERIC INTERFACES
 interface Box<Type> {
   content: Type;
@@ -29,4 +29,36 @@ function logLength<T extends hasLength>(arg: T): void {
 
 logLength("Hello");
 logLength({ length: 12 });
+//*/
+
+//*/
+// KEYOFF OPERATOR
+// The keyof type operator in TypeScript is a powerful tool that allows us to create new types from existing types by extracting the keys (property names) of an object type as a union of string literals.
+
+//! Extracting Keys from an Object Type
+
+type User = {
+  id: number;
+  name: string;
+  age: number;
+};
+
+//! Just an example of what keyOf operator gives us
+type UserKeys = keyof User; // "id" | "name" | "age"
+
+let key: UserKeys;
+key = "id"; // ✅ Allowed
+key = "name"; // ✅ Allowed
+// key = "email"; // ❌ Error: Property "email" does not exist on type "UserKeys"
+
+//! Using keyof in Function Parameters and Making a usecase for type U
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+const user: User = { id: 1, name: "Alice", age: 30 };
+
+console.log(getProperty(user, "name")); // ✅ "Alice"
+console.log(getProperty(user, "age")); // ✅ 30
+// console.log(getProperty(user, "email")); // ❌ Error: Argument of type '"email"' is not assignable
 //*/
